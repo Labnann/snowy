bf30b863fd36f7edc3a0e59a65409170887c709b
Labnan <khalidmasum@iut-dhaka.edu>
2021-12-21 15:53:59 +0600

    Set Controls

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,52 @@
 import {
+    AxesHelper,
     DirectionalLight, Mesh,
     MeshBasicMaterial,
+    MeshPhongMaterial,
     PerspectiveCamera,
+    PlaneBufferGeometry,
     Renderer, Scene,
     SphereBufferGeometry,
     WebGLRenderer
 } from "three";
-import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 let canvas = document.querySelector("#c") as HTMLCanvasElement;
-const renderer = new WebGLRenderer({canvas});
+const renderer = new WebGLRenderer({ canvas });
 const camera = new PerspectiveCamera(40, 2, 0.1, 1000);
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.target.set(0, 5, 0);
-controls.update();
 
+{
+    controls.target.set(0, 10, 0);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.05;
+    controls.maxPolarAngle = Math.PI / 2;
 
-camera.position.set(0, 20, 100);
-camera.lookAt(0, 0, 0);
+    controls.minDistance = 100;
+    controls.maxDistance = 500;
+}
+
+camera.position.set(0, 10, 30);
+//camera.lookAt(0, 0, 0);
 
 const scene = new Scene();
+{
+    const plane = new Mesh(new PlaneBufferGeometry(2000, 2000, 2, 2), new MeshBasicMaterial({ color: 0x999999 }));
+    scene.add(plane);
+    plane.rotateX(-Math.PI / 2)
+    // plane.position.set(0,-10,0);
 
-const sphere = new Mesh(new SphereBufferGeometry(20, 1000), new MeshBasicMaterial({color: 0x449900}));
-scene.add(sphere);
+}
+
+{
+    const axesHelper = new AxesHelper(30);
+    scene.add(axesHelper)
+}
 
 {
 
     let light = new DirectionalLight("0xFFFFFF", 1);
+    light.position.set(0, 50, 0);
     scene.add(light);
 }
 
@@ -52,6 +72,8 @@ function render() {
         camera.aspect = canvas.clientWidth / canvas.clientHeight;
         camera.updateProjectionMatrix();
     }
+
+    controls.update();
 
     renderer.render(scene, camera);
 
